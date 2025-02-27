d02dc962909d73f9ba5e20f47a3012aa7db5ae96
serhii.suzanskyi <serhii.suzanskyi@navinfo.eu>
2025-02-27 19:38:35 +0100

    fixed ui execution in headless mode

--- utilities/driver_factory.py
+++ utilities/driver_factory.py
@@ -7,22 +7,26 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
+
 class DriverFactory:
     @staticmethod
     def get_driver(browser_name):
         if browser_name.lower() == "chrome":
             options = webdriver.ChromeOptions()
-            options.add_argument("--start-maximized")  # Open in maximized mode
+            options.add_argument("--headless=new")  # Run in headless mode
             options.add_argument("--disable-gpu")  # Disable GPU (fixes some rendering issues)
             options.add_argument("--no-sandbox")  # Bypass OS security model (for Docker/Linux)
             options.add_argument("--disable-dev-shm-usage")  # Prevent crashes in Docker/Linux
+            options.add_argument("--remote-debugging-port=9222")  # Avoid session issues
+            options.add_argument("--user-data-dir=/tmp/chrome-user-data")  # Unique session directory
             options.add_experimental_option("excludeSwitches", ["enable-automation"])  # Avoid detection
             options.add_experimental_option("useAutomationExtension", False)  # Disable automation extension
 
             return webdriver.Chrome(
                 service=Service(ChromeDriverManager().install()), options=options
             )
+
         elif browser_name.lower() == "firefox":
             return webdriver.Firefox(service=Service(GeckoDriverManager().install()))
         else:
-            raise ValueError(f"Browser {browser_name} is not supported") 
+            raise ValueError(f"Browser {browser_name} is not supported")
